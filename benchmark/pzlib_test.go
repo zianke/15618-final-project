08afd08845275161adebb552e722457de2b59b88
pengyun961012 <pengyun@umich.edu>
2020-05-06 17:16:34 -0400

    benchmark testcases and driver program

---
+++ benchmark/pzlib_test.go
@@ -0,0 +1,434 @@
+package pzlib_test
+
+import (
+	"bytes"
+	"compress/zlib"
+	"fmt"
+	zlib2 "github.com/klauspost/compress/zlib"
+	"io"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"pgzip-test/pzlib"
+	pzlib2 "pgzip-test/pzlib-std"
+	"testing"
+)
+
+func generateJson(size int) []byte {
+	dat, _ := ioutil.ReadFile("testdata/test.json")
+	dl := len(dat)
+	testbuf := make([]byte, size)
+	for j := 0; j < size; j += dl {
+		if j+dl < size {
+			copy(testbuf[j:j+dl], dat)
+		} else {
+			copy(testbuf[j:], dat[:size-j])
+		}
+	}
+	return testbuf
+}
+
+func generateText(size int) []byte {
+	dat, _ := ioutil.ReadFile("testdata/hamlet.txt")
+	dl := len(dat)
+	testbuf := make([]byte, size)
+	for j := 0; j < size; j += dl {
+		if j+dl < size {
+			copy(testbuf[j:j+dl], dat)
+		} else {
+			copy(testbuf[j:], dat[:size-j])
+		}
+	}
+	return testbuf
+}
+
+func generateRandom(size int) []byte {
+	rand.Seed(15618)
+	testbuf := make([]byte, size)
+	for idx := range testbuf {
+		testbuf[idx] = byte(65 + rand.Intn(32))
+	}
+	return testbuf
+}
+
+func generateDuplicate(size int) []byte {
+	testbuf := make([]byte, size)
+	for idx := range testbuf {
+		testbuf[idx] = byte(65)
+	}
+	return testbuf
+}
+
+func benchmarkJson(i int, b *testing.B) {
+	testbuf := generateJson(i)
+
+	b.Run("zlib", func(b *testing.B) {
+		b.SetBytes(int64(len(testbuf)))
+		for n := 0; n < b.N; n++ {
+			br := bytes.NewBuffer(testbuf)
+			var buf bytes.Buffer
+			w, _ := zlib.NewWriterLevel(&buf, 6)
+			io.Copy(w, br)
+			w.Close()
+			r, _ := zlib.NewReader(&buf)
+			ioutil.ReadAll(r)
+		}
+	})
+
+	b.Run("zlib2", func(b *testing.B) {
+		b.SetBytes(int64(len(testbuf)))
+		for n := 0; n < b.N; n++ {
+			br := bytes.NewBuffer(testbuf)
+			var buf bytes.Buffer
+			w, _ := zlib2.NewWriterLevel(&buf, 6)
+			io.Copy(w, br)
+			w.Close()
+			r, _ := zlib2.NewReader(&buf)
+			ioutil.ReadAll(r)
+		}
+	})
+
+	b.Run("pzlib", func(b *testing.B) {
+		b.SetBytes(int64(len(testbuf)))
+		for n := 0; n < b.N; n++ {
+			br := bytes.NewBuffer(testbuf)
+			var buf bytes.Buffer
+			w, _ := pzlib.NewWriterLevel(&buf, 6)
+			io.Copy(w, br)
+			w.Close()
+			r, _ := pzlib.NewReader(&buf)
+			ioutil.ReadAll(r)
+		}
+	})
+
+	b.Run("pzlib2", func(b *testing.B) {
+		b.SetBytes(int64(len(testbuf)))
+		for n := 0; n < b.N; n++ {
+			br := bytes.NewBuffer(testbuf)
+			var buf bytes.Buffer
+			w, _ := pzlib2.NewWriterLevel(&buf, 6)
+			io.Copy(w, br)
+			w.Close()
+			r, _ := pzlib2.NewReader(&buf)
+			ioutil.ReadAll(r)
+		}
+	})
+}
+
+func BenchmarkJson1K(b *testing.B)   { benchmarkJson(1000, b) }
+func BenchmarkJson10K(b *testing.B)  { benchmarkJson(10000, b) }
+func BenchmarkJson100K(b *testing.B) { benchmarkJson(100000, b) }
+func BenchmarkJson1M(b *testing.B)   { benchmarkJson(1000000, b) }
+func BenchmarkJson10M(b *testing.B)  { benchmarkJson(10000000, b) }
+func BenchmarkJson100M(b *testing.B) { benchmarkJson(100000000, b) }
+func BenchmarkJson1G(b *testing.B)   { benchmarkJson(1000000000, b) }
+
+func BenchmarkFileSize(b *testing.B) {
+	for _, fileSize := range []int{1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000} {
+		testbuf := generateJson(fileSize)
+
+		b.Run(fmt.Sprintf("zlib-%d", fileSize), func(b *testing.B) {
+			b.SetBytes(int64(len(testbuf)))
+			for n := 0; n < b.N; n++ {
+				br := bytes.NewBuffer(testbuf)
+				var buf bytes.Buffer
+				w, _ := zlib.NewWriterLevel(&buf, 6)
+				io.Copy(w, br)
+				w.Close()
+				r, _ := zlib.NewReader(&buf)
+				ioutil.ReadAll(r)
+			}
+		})
+
+		for _, threadNum := range []int{1, 2, 4, 8, 16, 32, 64} {
+			b.Run(fmt.Sprintf("pzlib-%d-%d", fileSize, threadNum), func(b *testing.B) {
+				b.SetBytes(int64(len(testbuf)))
+				for n := 0; n < b.N; n++ {
+					br := bytes.NewBuffer(testbuf)
+					var buf bytes.Buffer
+					w, _ := pzlib.NewWriterLevel(&buf, 6)
+					w.SetConcurrency(1<<20, threadNum)
+					io.Copy(w, br)
+					w.Close()
+					r, _ := pzlib.NewReader(&buf)
+					ioutil.ReadAll(r)
+				}
+			})
+		}
+	}
+}
+
+func BenchmarkBlockSize(b *testing.B) {
+	testbuf := generateJson(100000000)
+
+	b.Run("zlib", func(b *testing.B) {
+		b.SetBytes(int64(len(testbuf)))
+		for n := 0; n < b.N; n++ {
+			br := bytes.NewBuffer(testbuf)
+			var buf bytes.Buffer
+			w, _ := zlib.NewWriterLevel(&buf, 6)
+			io.Copy(w, br)
+			w.Close()
+			r, _ := zlib.NewReader(&buf)
+			ioutil.ReadAll(r)
+		}
+	})
+
+	for _, blockSize := range []int{1 << 14, 1 << 16, 1 << 18, 1 << 20, 1 << 22, 1 << 24} {
+		for _, threadNum := range []int{1, 2, 4, 8, 16, 32, 64} {
+			b.Run(fmt.Sprintf("pzlib-%d-%d", blockSize, threadNum), func(b *testing.B) {
+				b.SetBytes(int64(len(testbuf)))
+				for n := 0; n < b.N; n++ {
+					br := bytes.NewBuffer(testbuf)
+					var buf bytes.Buffer
+					w, _ := pzlib.NewWriterLevel(&buf, 6)
+					w.SetConcurrency(blockSize, threadNum)
+					io.Copy(w, br)
+					w.Close()
+					r, _ := pzlib.NewReader(&buf)
+					ioutil.ReadAll(r)
+				}
+			})
+		}
+	}
+}
+
+func benchmarkLevel(level int, b *testing.B) {
+	testbuf := generateJson(100000000)
+
+	b.SetBytes(int64(len(testbuf)))
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		br := bytes.NewBuffer(testbuf)
+		var buf bytes.Buffer
+		w, _ := pzlib.NewWriterLevel(&buf, level)
+		io.Copy(w, br)
+		w.Close()
+		r, _ := pzlib.NewReader(&buf)
+		ioutil.ReadAll(r)
+	}
+}
+
+func BenchmarkLevel1(b *testing.B) { benchmarkLevel(1, b) }
+func BenchmarkLevel2(b *testing.B) { benchmarkLevel(2, b) }
+func BenchmarkLevel3(b *testing.B) { benchmarkLevel(3, b) }
+func BenchmarkLevel4(b *testing.B) { benchmarkLevel(4, b) }
+func BenchmarkLevel5(b *testing.B) { benchmarkLevel(5, b) }
+func BenchmarkLevel6(b *testing.B) { benchmarkLevel(6, b) }
+func BenchmarkLevel7(b *testing.B) { benchmarkLevel(7, b) }
+func BenchmarkLevel8(b *testing.B) { benchmarkLevel(8, b) }
+func BenchmarkLevel9(b *testing.B) { benchmarkLevel(9, b) }
+
+func testLevel(level int, t *testing.T) {
+	testbuf := generateJson(100000000)
+
+	br := bytes.NewBuffer(testbuf)
+	var buf bytes.Buffer
+	w, _ := pzlib.NewWriterLevel(&buf, level)
+	io.Copy(w, br)
+	w.Close()
+
+	log.Printf("[Level %d] Original size: %d bytes; Size after compression: %d bytes", level, len(testbuf), len(buf.Bytes()))
+}
+
+func TestLevel1(t *testing.T) { testLevel(1, t) }
+func TestLevel2(t *testing.T) { testLevel(2, t) }
+func TestLevel3(t *testing.T) { testLevel(3, t) }
+func TestLevel4(t *testing.T) { testLevel(4, t) }
+func TestLevel5(t *testing.T) { testLevel(5, t) }
+func TestLevel6(t *testing.T) { testLevel(6, t) }
+func TestLevel7(t *testing.T) { testLevel(7, t) }
+func TestLevel8(t *testing.T) { testLevel(8, t) }
+func TestLevel9(t *testing.T) { testLevel(9, t) }
+
+func BenchmarkDuplicate(b *testing.B) {
+	testbuf := generateDuplicate(100000000)
+
+	b.Run("zlib", func(b *testing.B) {
+		b.SetBytes(int64(len(testbuf)))
+		for n := 0; n < b.N; n++ {
+			br := bytes.NewBuffer(testbuf)
+			var buf bytes.Buffer
+			w, _ := zlib.NewWriterLevel(&buf, 6)
+			io.Copy(w, br)
+			w.Close()
+			r, _ := zlib.NewReader(&buf)
+			ioutil.ReadAll(r)
+		}
+	})
+
+	b.Run("pzlib", func(b *testing.B) {
+		b.SetBytes(int64(len(testbuf)))
+		for n := 0; n < b.N; n++ {
+			br := bytes.NewBuffer(testbuf)
+			var buf bytes.Buffer
+			w, _ := pzlib.NewWriterLevel(&buf, 6)
+			io.Copy(w, br)
+			w.Close()
+			r, _ := pzlib.NewReader(&buf)
+			ioutil.ReadAll(r)
+		}
+	})
+}
+
+func TestDuplicate(t *testing.T) {
+	testbuf := generateDuplicate(100000000)
+
+	br1 := bytes.NewBuffer(testbuf)
+	var buf1 bytes.Buffer
+	w1, _ := zlib.NewWriterLevel(&buf1, 6)
+	io.Copy(w1, br1)
+	w1.Close()
+
+	br2 := bytes.NewBuffer(testbuf)
+	var buf2 bytes.Buffer
+	w2, _ := pzlib.NewWriterLevel(&buf2, 6)
+	io.Copy(w2, br2)
+	w2.Close()
+
+	log.Printf("[Duplicate] Original size: %d bytes; Size after compression by zlib: %d bytes; Size after compression by pzlib: %d bytes", len(testbuf), len(buf1.Bytes()), len(buf2.Bytes()))
+}
+
+func BenchmarkRandom(b *testing.B) {
+	testbuf := generateRandom(100000000)
+
+	b.Run("zlib", func(b *testing.B) {
+		b.SetBytes(int64(len(testbuf)))
+		for n := 0; n < b.N; n++ {
+			br := bytes.NewBuffer(testbuf)
+			var buf bytes.Buffer
+			w, _ := zlib.NewWriterLevel(&buf, 6)
+			io.Copy(w, br)
+			w.Close()
+			r, _ := zlib.NewReader(&buf)
+			ioutil.ReadAll(r)
+		}
+	})
+
+	b.Run("pzlib", func(b *testing.B) {
+		b.SetBytes(int64(len(testbuf)))
+		for n := 0; n < b.N; n++ {
+			br := bytes.NewBuffer(testbuf)
+			var buf bytes.Buffer
+			w, _ := pzlib.NewWriterLevel(&buf, 6)
+			io.Copy(w, br)
+			w.Close()
+			r, _ := pzlib.NewReader(&buf)
+			ioutil.ReadAll(r)
+		}
+	})
+}
+
+func TestRandom(t *testing.T) {
+	testbuf := generateRandom(100000000)
+
+	br1 := bytes.NewBuffer(testbuf)
+	var buf1 bytes.Buffer
+	w1, _ := zlib.NewWriterLevel(&buf1, 6)
+	io.Copy(w1, br1)
+	w1.Close()
+
+	br2 := bytes.NewBuffer(testbuf)
+	var buf2 bytes.Buffer
+	w2, _ := pzlib.NewWriterLevel(&buf2, 6)
+	io.Copy(w2, br2)
+	w2.Close()
+
+	log.Printf("[Random] Original size: %d bytes; Size after compression by zlib: %d bytes; Size after compression by pzlib: %d bytes", len(testbuf), len(buf1.Bytes()), len(buf2.Bytes()))
+}
+
+func BenchmarkJson(b *testing.B) {
+	testbuf := generateJson(100000000)
+
+	b.Run("zlib", func(b *testing.B) {
+		b.SetBytes(int64(len(testbuf)))
+		for n := 0; n < b.N; n++ {
+			br := bytes.NewBuffer(testbuf)
+			var buf bytes.Buffer
+			w, _ := zlib.NewWriterLevel(&buf, 6)
+			io.Copy(w, br)
+			w.Close()
+			r, _ := zlib.NewReader(&buf)
+			ioutil.ReadAll(r)
+		}
+	})
+
+	b.Run("pzlib", func(b *testing.B) {
+		b.SetBytes(int64(len(testbuf)))
+		for n := 0; n < b.N; n++ {
+			br := bytes.NewBuffer(testbuf)
+			var buf bytes.Buffer
+			w, _ := pzlib.NewWriterLevel(&buf, 6)
+			io.Copy(w, br)
+			w.Close()
+			r, _ := pzlib.NewReader(&buf)
+			ioutil.ReadAll(r)
+		}
+	})
+}
+
+func TestJson(t *testing.T) {
+	testbuf := generateJson(100000000)
+
+	br1 := bytes.NewBuffer(testbuf)
+	var buf1 bytes.Buffer
+	w1, _ := zlib.NewWriterLevel(&buf1, 6)
+	io.Copy(w1, br1)
+	w1.Close()
+
+	br2 := bytes.NewBuffer(testbuf)
+	var buf2 bytes.Buffer
+	w2, _ := pzlib.NewWriterLevel(&buf2, 6)
+	io.Copy(w2, br2)
+	w2.Close()
+
+	log.Printf("[JSON] Original size: %d bytes; Size after compression by zlib: %d bytes; Size after compression by pzlib: %d bytes", len(testbuf), len(buf1.Bytes()), len(buf2.Bytes()))
+}
+
+func BenchmarkText(b *testing.B) {
+	testbuf := generateText(100000000)
+
+	b.Run("zlib", func(b *testing.B) {
+		b.SetBytes(int64(len(testbuf)))
+		for n := 0; n < b.N; n++ {
+			br := bytes.NewBuffer(testbuf)
+			var buf bytes.Buffer
+			w, _ := zlib.NewWriterLevel(&buf, 6)
+			io.Copy(w, br)
+			w.Close()
+			r, _ := zlib.NewReader(&buf)
+			ioutil.ReadAll(r)
+		}
+	})
+
+	b.Run("pzlib", func(b *testing.B) {
+		b.SetBytes(int64(len(testbuf)))
+		for n := 0; n < b.N; n++ {
+			br := bytes.NewBuffer(testbuf)
+			var buf bytes.Buffer
+			w, _ := pzlib.NewWriterLevel(&buf, 6)
+			io.Copy(w, br)
+			w.Close()
+			r, _ := pzlib.NewReader(&buf)
+			ioutil.ReadAll(r)
+		}
+	})
+}
+
+func TestText(t *testing.T) {
+	testbuf := generateText(100000000)
+
+	br1 := bytes.NewBuffer(testbuf)
+	var buf1 bytes.Buffer
+	w1, _ := zlib.NewWriterLevel(&buf1, 6)
+	io.Copy(w1, br1)
+	w1.Close()
+
+	br2 := bytes.NewBuffer(testbuf)
+	var buf2 bytes.Buffer
+	w2, _ := pzlib.NewWriterLevel(&buf2, 6)
+	io.Copy(w2, br2)
+	w2.Close()
+
+	log.Printf("[Text] Original size: %d bytes; Size after compression by zlib: %d bytes; Size after compression by pzlib: %d bytes", len(testbuf), len(buf1.Bytes()), len(buf2.Bytes()))
+}
